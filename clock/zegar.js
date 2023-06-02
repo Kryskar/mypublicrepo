a9857d296b8361120d3a501196bcbace79b9c3c9
Kryskar <132471940+Kryskar@users.noreply.github.com>
2023-06-02 17:40:22 +0200

    clock update

--- clock/zegar.js
+++ clock/zegar.js
@@ -2,20 +2,36 @@ let secondHand = document.getElementById("secondHand")
 let minuteHand = document.getElementById("minuteHand")
 let hourHand = document.getElementById("hourHand")
 
-let s = 0, m=0, h=0; 
-function seconds(){
-    secondHand.style.transform = "rotate("+s+"deg)"
-    s+=6
-}
-function minutes(){
-    minuteHand.style.transform = "rotate("+m+"deg)"
-    m+=6
-}
-function hours(){
-    hourHand.style.transform = "rotate("+h+"deg)"
-    h+=30
+let secondsDeg, minutesDeg, hoursDeg
+function time (){
+    let date = new Date();
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    if (secondsDeg >= 354){
+        secondsDeg+=6
+    } else {
+     secondsDeg = (seconds/60)*360
+    }
+    if (minutesDeg >= 354){
+        minutesDeg+=0.1
+    } else {
+     minutesDeg = ((minutes+ seconds/60)/60)*360
+    }
+    if (hoursDeg>=354){
+        hoursDeg+=0.1/12
+    }
+     hoursDeg = ((hours + minutesDeg/360)/12)*360
+
+    secondHand.style.transform = `rotate(${secondsDeg}deg)`
+    minuteHand.style.transform = `rotate(${minutesDeg}deg)`
+    hourHand.style.transform = `rotate(${hoursDeg}deg)`
+
 }
 
-setInterval(()=>{seconds()},1000);
-setInterval(()=>{minutes()},1000*60);
-setInterval(()=>{hours()},1000*60*60);
+time();
+
+setInterval(time,1000);
+setTimeout(function() {
+    location.reload();
+  }, 1000*60*15);
