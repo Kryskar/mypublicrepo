ed073c46615ff4431e1feb6f2b1cb937f73b24e3
Kryskar <132471940+Kryskar@users.noreply.github.com>
2023-06-05 21:57:06 +0200

    updates and fixes

--- lightbox/script.js
+++ lightbox/script.js
@@ -12,22 +12,30 @@ button.checked = (theme === "dark");
 }
 button.addEventListener("change",switchTheme)
 
+
 const lightbox = document.querySelector("#lightbox")
 const yesButton = document.querySelector(".yes")
 const noButton = document.querySelector(".no")
 const page = document.querySelector(".page")
+const body = document.querySelector("body")
 
 //if(localStorage.is18){
-//    lightbox.style.display = "none";
+//    above18();
 //}
 
-
 yesButton.addEventListener("click",()=>{
-    lightbox.style.display = "none";
-    page.style.filter = "none"
+    above18();
     localStorage.setItem("is18", true)
 })
 
 noButton.addEventListener("click",()=>{
     window.location.replace("https://previews.123rf.com/images/vospalej/vospalej0911/vospalej091100039/5993883-no-entry-when-you-are-under-18.jpg")
 })
+
+function above18(){
+    lightbox.style.display = "none";
+    page.style.filter = "none"
+    page.style.pointerEvents = "auto"
+    page.style.userSelect = "auto"
+    body.style.backgroundColor="var(--bg-color)"
+}
